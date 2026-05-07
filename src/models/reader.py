d760c60d44655caa1e95fe40a6b26c701d7ad0bf
OleksandrPashenko322 <pashenkooleksandr69@gmail.com>
2026-05-08 02:15:35 +0300

    feat: додати модель Reader для системи реєстрації

--- src/models/reader.py
+++ src/models/reader.py
@@ -1,1 +1,7 @@
-#user auth#
+# src/models/reader.py
+class Reader:
+    def __init__(self, name, email, library_card_number):
+        self.name = name
+        self.email = email
+        self.library_card_number = library_card_number
+        self.is_active = True
